020f95d7e48f950c2138ba2939e72ad6cad588e9
CestmirKolarik <CestmirKolarik@users.noreply.github.com>
2021-02-03 19:06:42 +0100

    Oprava kódu
    Byl přidán správný kód.

--- ArduinoIDE/LEDka_postupne_roznuti/LEDKa_postupne_roznuti.ino
+++ ArduinoIDE/LEDka_postupne_roznuti/LEDKa_postupne_roznuti.ino
@@ -1,28 +1,15 @@
-  #define red 12 //D6
-  #define gre 13 //D7
-  #define blu 15 //D8
-
-  int prodleva = 1500; //prodleva sekunda a půl
+  #define ledka 5 //D1
 
 void setup() {
 
-  pinMode(red, OUTPUT);
-  pinMode(gre, OUTPUT);
-  pinMode(blu, OUTPUT);
+  pinMode(ledka, OUTPUT);
 }
 
 void loop() {
 
-  //červená - #FF0000 - (255, 0, 0)
-  digitalWrite(red, HIGH);
-  digitalWrite(gre, LOW);
-  digitalWrite(blu, LOW);
-  delay(prodleva);
-
-  //žlutá - #FFFF00 - (255, 255, 0)
-  digitalWrite(red, HIGH);
-  digitalWrite(gre, HIGH);
-  digitalWrite(blu, LOW);
-  delay(prodleva);
-
+for(int i = 0; i < 1024; i++)
+  {
+    analogWrite(ledka, i);
+    delay(5);
+  }
 }
