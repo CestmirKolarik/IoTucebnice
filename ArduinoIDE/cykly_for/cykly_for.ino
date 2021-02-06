b4bfe86d7650ce6b74a9ea36aa401c4d671b8384
CestmirKolarik <CestmirKolarik@users.noreply.github.com>
2021-02-06 20:43:15 +0100

    for cyklus

---
+++ ArduinoIDE/cykly_for/cykly_for.ino
@@ -0,0 +1,16 @@
+  int prodleva = 100; //prodleva bude 0,1 s  
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("Sériová komunikace funguje");
+}
+
+void loop() {
+  //10× vypsat Ahoj
+  for(int i = 0; i <10; i++)
+  {
+    Serial.println("Ahoj");
+  }
+  
+}
