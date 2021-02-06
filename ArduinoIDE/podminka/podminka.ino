358841af48a4f76ff36c8905605efac8c87ea631
CestmirKolarik <CestmirKolarik@users.noreply.github.com>
2021-02-06 20:52:28 +0100

    kód pro podmínku

---
+++ ArduinoIDE/podminka/podminka.ino
@@ -0,0 +1,23 @@
+  int prodleva = 100; //prodleva bude 0,1 s  
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  Serial.println("Sériová komunikace funguje");
+}
+
+void loop() {
+
+  for(int cislo = 1; cislo <=20; cislo++)
+  {
+    if(cislo > 10)
+    {
+      Serial.println("Číslo je větší než 10");
+    }
+      else
+    {
+      Serial.println("Číslo není větší než 10")
+    }
+  }
+  
+}
