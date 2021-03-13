681ab6aebb62d6db735b5b9643cb8c7ed4974053
CestmirKolarik <CestmirKolarik@users.noreply.github.com>
2021-03-13 14:26:21 +0100

    Další změny

---
+++ ArduinoIDE/LCD/LCD.ino
@@ -0,0 +1,20 @@
+//Import knihoven
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// Nastevení adresy displeje a počet znaků na řádek a počet řádků
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup()
+{
+	lcd.begin();
+
+	// Zapnutí podstvícení a výpis zprávy
+	lcd.backlight();
+	lcd.print("Hrajeme si s IoT");
+}
+
+void loop()
+{
+
+}
