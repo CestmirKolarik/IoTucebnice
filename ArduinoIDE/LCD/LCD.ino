4c14f8343d462b2a90bbabc0d6db337ff32f6f52
CestmirKolarik <CestmirKolarik@users.noreply.github.com>
2021-03-13 14:28:39 +0100

    formátování

--- ArduinoIDE/LCD/LCD.ino
+++ ArduinoIDE/LCD/LCD.ino
@@ -1,9 +1,9 @@
-//Import knihoven
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
+  //Import knihoven
+  #include <Wire.h> 
+  #include <LiquidCrystal_I2C.h>
 
-// Nastevení adresy displeje a počet znaků na řádek a počet řádků
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+  // Nastevení adresy displeje a počet znaků na řádek a počet řádků
+  LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
